2aa72d8f0ba6b7adeb76e3a54f7613ab680008e0
Lucas Honorio <110117686+lucashon@users.noreply.github.com>
2023-05-05 17:12:42 -0300

    Add files via upload

---
+++ expenses-chart-component-main/app.js
@@ -0,0 +1,76 @@
+document.querySelector('.grafico1').addEventListener('mouseover',()=>{
+    document.querySelector('.grafico1').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.amount').textContent = '$17.45';
+
+})
+document.querySelector('.grafico2').addEventListener('mouseover',()=>{
+    document.querySelector('.grafico2').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.amount2').textContent = '$34.91';
+
+})
+document.querySelector('.grafico3').addEventListener('mouseover',()=>{
+    document.querySelector('.grafico3').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.amount3').textContent = '$52.36';
+
+})
+document.querySelector('.grafico4').addEventListener('mouseover',()=>{
+    document.querySelector('.grafico4').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.amount4').textContent = '$31.07';
+
+})
+document.querySelector('.grafico5').addEventListener('mouseover',()=>{
+    document.querySelector('.grafico5').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.amount5').textContent = '$23.39';
+
+})
+document.querySelector('.grafico6').addEventListener('mouseover',()=>{
+    document.querySelector('.grafico6').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.amount6').textContent = '$43.28';
+
+})
+document.querySelector('.grafico7').addEventListener('mouseover',()=>{
+    document.querySelector('.grafico7').style.background = 'hsl(186, 34%, 60%)';
+    document.querySelector('.amount7').textContent = '$25.48';
+
+})
+
+
+document.querySelector('.grafico1').addEventListener('mouseout',()=>{
+    document.querySelector('.grafico1').style.background = 'hsl(10, 79%, 65%)';
+    document.querySelector('.amount').textContent = '';
+
+})
+document.querySelector('.grafico2').addEventListener('mouseout',()=>{
+    document.querySelector('.grafico2').style.background = 'hsl(10, 79%, 65%)';
+    document.querySelector('.amount2').textContent = '';
+
+})
+document.querySelector('.grafico3').addEventListener('mouseout',()=>{
+    document.querySelector('.grafico3').style.background = 'hsl(10, 79%, 65%)';
+    document.querySelector('.amount3').textContent = '';
+
+})
+document.querySelector('.grafico4').addEventListener('mouseout',()=>{
+    document.querySelector('.grafico4').style.background = 'hsl(10, 79%, 65%)';
+    document.querySelector('.amount4').textContent = '';
+
+})
+document.querySelector('.grafico5').addEventListener('mouseout',()=>{
+    document.querySelector('.grafico5').style.background = 'hsl(10, 79%, 65%)';
+    document.querySelector('.amount5').textContent = '';
+
+})
+document.querySelector('.grafico6').addEventListener('mouseout',()=>{
+    document.querySelector('.grafico6').style.background = 'hsl(10, 79%, 65%)';
+    document.querySelector('.amount6').textContent = '';
+
+})
+document.querySelector('.grafico7').addEventListener('mouseout',()=>{
+    document.querySelector('.grafico7').style.background = 'hsl(10, 79%, 65%)';
+    document.querySelector('.amount7').textContent = '';
+
+})
+
+   
+
+
